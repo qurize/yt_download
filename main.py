54e216c9b8f34dfd4c5ab9023f5c2b034ff349ba
IREM <appoop@gmail.com>
2022-10-14 12:15:52 +0700

    v0.1

--- main.py
+++ main.py
@@ -1,19 +1,59 @@
-v = "v.0-"
-# print(v)
+v = "v.01"
+import PySimpleGUI as sg
 from pytube import YouTube
+import os
 
-url = "https://music.youtube.com/watch?v=J1xXu-18Zb0&list=OLAK5uy_mNuiuW4Eg8WiWEDY4r8Tjrh6CiinagQvg"
+sg.theme('DarkTeal9')
 
-vdo = YouTube(url)
+layout = [
+    [sg.Text('Youtube Audio Download : ')],
+    [sg.Text('Save Path', size=(10, 1)), sg.InputText(key='-path-'), sg.Button("Path", size=(5, 1))],
+    [sg.Text('URL', size=(10, 1)), sg.InputText(key='-url-')],
+    [sg.Button("Download", size=(10, 1)), sg.Exit()]]
 
-print(vdo.title)
-print(vdo.thumbnail_url)
+window = sg.Window('Simple data entry form', layout)
 
-#vdo = vdo.streams.get_highest_resolution()
-audio = vdo.streams.get_audio_only()
-# vdo = vdo.streams.first()
-# vdo = vdo.streams.last()
+while True:
+    event, values = window.read()
+    print(event)
+    print(values)
+    print(values["-url-"])
+    if event == sg.WIN_CLOSED or event == 'Exit':
+        break
+    if event == "Path":
+        path = sg.popup_get_folder('Please enter a folder name')
+        window["-path-"].update(path)
 
-#vdo.download()
-audio.download()
-print("End")
+        pass
+    if event == 'Download':
+        if values["-path-"] != "" and values["-url-"] != "":
+            path = values["-path-"]
+            print(path.replace("/", "\\"))
+
+            video = YouTube(values["-url-"])
+            print("title is : ", video.title)
+            video.streams.filter(only_audio=True).first().download(path, filename="TemporaryName.Mp4")
+
+            # remove caracters (" | , / \ ..... ) from video title
+
+            VideoTitle = video.title
+            VideoTitle = VideoTitle.replace('\"', " ")
+            VideoTitle = VideoTitle.replace('|', " ")
+            VideoTitle = VideoTitle.replace(',', " ")
+            VideoTitle = VideoTitle.replace('/', " ")
+            VideoTitle = VideoTitle.replace('\\', " ")
+            VideoTitle = VideoTitle.replace(':', " ")
+            VideoTitle = VideoTitle.replace('*', " ")
+            VideoTitle = VideoTitle.replace('?', " ")
+            VideoTitle = VideoTitle.replace('<', " ")
+            VideoTitle = VideoTitle.replace('>', " ")
+            VideoTitle = VideoTitle.replace('#', " ")
+
+            # change name and converting Mp4 to Mp3
+            my_file = path + "\\" + "TemporaryName.mp4"
+            base = path + "\\" + VideoTitle
+            print("New Video Title is :" + VideoTitle)
+            os.rename(my_file, base + '.mp3')
+            print(video.title, ' \nhas been successfully downloaded as MP3')
+
+window.close()
